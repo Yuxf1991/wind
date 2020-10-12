4f749027cd7217c4ccdbb6623ae6f6cd029cb723
Yuxf1991 <yuxf1991@gmail.com>
2020-10-12 19:55:52 +0800

    CountDownLatch

--- src/base/include/CountDownLatch.h
+++ src/base/include/CountDownLatch.h
@@ -8,7 +8,36 @@
 #include "Mutex.h"
 #include "base/Condition.h"
 
+namespace wind {
+    class CountDownLatch : NonCopyable {
+    public:
+        explicit CountDownLatch(int count) : m_count(count) {}
+        ~CountDownLatch() = default;
 
+        int getCount() const {
+            LockGuard<Mutex> lock(m_mutex);
+            return m_count;
+        }
+
+        void countDown() {
+            LockGuard<Mutex> lock(m_mutex);
+            m_count--;
+            if (m_count == 0) {
+                m_condition.notifyAll();
+            }
+        }
+
+        void wait() {
+            UniqueLock<Mutex> lock(m_mutex);
+            m_condition.wait(lock, [this]() { m_count <= 0; });
+        }
+
+    private:
+        mutable Mutex m_mutex;
+        int m_count GUARDED_BY(m_mutex);
+        Condition m_condition GUARDED_BY(m_mutex);
+    };
+} // namespace wind
 
 
 #endif//WIND_COUNTDOWNLATCH_H
