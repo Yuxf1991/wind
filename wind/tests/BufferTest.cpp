3bf0c8e8cc685fb2843644be78652b85bc36819b
Yuxf1991 <Yuxf1991@gmail.com>
2021-12-07 23:10:10 +0800

    add buffer_test cases.

--- wind/tests/BufferTest.cpp
+++ wind/tests/BufferTest.cpp
@@ -46,5 +46,35 @@ TEST(BufferTest, AppendAndResetTest)
     EXPECT_EQ(buf0.curr(), buf0.data());
     EXPECT_EQ(buf0.end(), buf0.data() + DEFAULT_BUFFER_SIZE);
     EXPECT_EQ(buf0.length(), 0);
+
+    string tmp1(", my name is robot.");
+    buf0.append(tmp0);
+    buf0.append(tmp1);
+    std::cout << "After appending tmp0 + tmp1, the buf0 is " << buf0.toString()
+              << std::endl;
+    EXPECT_EQ(buf0.toString(), tmp0 + tmp1);
+    EXPECT_EQ(buf0.capacity(), DEFAULT_BUFFER_SIZE);
+    EXPECT_EQ(buf0.available(),
+              DEFAULT_BUFFER_SIZE - tmp0.length() - tmp1.length());
+    EXPECT_EQ(buf0.curr(), buf0.data() + tmp0.length() + tmp1.length());
+    EXPECT_EQ(buf0.end(), buf0.data() + DEFAULT_BUFFER_SIZE);
+    EXPECT_EQ(buf0.length(), tmp0.length() + tmp1.length());
+}
+
+TEST(BufferTest, AppendExceedTest)
+{
+    WIND_TEST_BEGIN(BufferTest, AppendExceedTest);
+
+    FixedSizeBuffer<10> buf;
+    string tmp = "hello world!";
+    buf.append(tmp);
+    std::cout << "After appending tmp, the buf is " << buf.toString()
+              << std::endl;
+    EXPECT_EQ(buf.toString(), tmp.substr(0, 10));
+    EXPECT_EQ(buf.capacity(), 10);
+    EXPECT_EQ(buf.available(), 0);
+    EXPECT_EQ(buf.curr(), buf.data() + 10);
+    EXPECT_EQ(buf.end(), buf.data() + 10);
+    EXPECT_EQ(buf.length(), 10);
 }
 } // namespace wind
